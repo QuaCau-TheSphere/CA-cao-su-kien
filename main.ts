09ab8937c3793a63a20899244298353fdb9d2a9e
ooker <ganuongphap@gmail.com>
2024-12-10 21:13:03 +0700

    feat: tạo cache trên GitHub Action

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ import { lấyApiCủaCalendar } from "./Code hỗ trợ/Google Calendar/Xác th
 import { nhậpSựKiện, xoáSựKiệnTươngLai } from "./Code hỗ trợ/Google Calendar/Google Calendar.ts";
 import { lấySựKiện } from "./Code hỗ trợ/Lấy sự kiện mới.ts";
 
-const dsSựKiện = await lấySựKiện();
 export const calendarApi = await lấyApiCủaCalendar();
+
 // await xoáSựKiệnTươngLai("Test ganu");
+const dsSựKiện = await lấySựKiện();
 await nhậpSựKiện(dsSựKiện);
